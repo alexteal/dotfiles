e4f0a3c99ec40c03f6a823535c856d077d4820fd
alexteal <ateal3235@gmail.com>
2022-07-07 10:18:31 -0400

    reformat and add p10k config

--- install.sh
+++ install.sh
@@ -4,19 +4,21 @@ while true; do
 
     read -p "Do you want to make backups of your current dotfiles? (y/n) " yn
 
-case $yn in 
-	[yY] ) echo making backups...;
-        mv -f ~/.zshrc ~/.zshrc.bak;
-        mv -f ~/.config/nvim/init.vim ~/.config/nvim/init.vim.bak;
-        mv -f ~/.tmux.conf ~/.tmux.conf.bak;
-		break;;
-	[nN] ) echo Proceeding without backups.;
-        break;;
-	* ) echo invalid response;;
-esac
+    case $yn in 
+        [yY] ) echo making backups...;
+            mv -f ~/.zshrc ~/.zshrc.bak;
+            mv -f ~/.config/nvim/init.vim ~/.config/nvim/init.vim.bak;
+            mv -f ~/.tmux.conf ~/.tmux.conf.bak;
+            mv -f ~/.p10k.zsh ~/.p10k.zsh.bak;
+            break;;
+        [nN] ) echo Proceeding without backups.;
+            break;;
+        * ) echo invalid response;;
+    esac
 
 done
 
 cp -f ./zsh/zshrc ~/.zshrc
 cp -f ./nvim/init.vim ~/.config/nvim/init.vim
 cp -f ./tmux/tmux.conf ~/.tmux.conf
+cp -f ./tmux/p10k.zsh ~/.p10k.zsh
