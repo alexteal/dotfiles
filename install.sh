919507a803a83feb947a4bd1f5632c7638a79853
alexteal <ateal3235@gmail.com>
2022-07-10 19:47:28 -0400

    updated installer

--- install.sh
+++ install.sh
@@ -15,10 +15,64 @@ while true; do
             break;;
         * ) echo invalid response;;
     esac
-
 done
 
 cp -f ./zsh/zshrc ~/.zshrc
 cp -f ./nvim/init.vim ~/.config/nvim/init.vim
 cp -f ./tmux/tmux.conf ~/.tmux.conf
 cp -f ./tmux/p10k.zsh ~/.p10k.zsh
+
+sleep 2
+# ensure brew
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+# ensure neovim
+which -s nvim
+if [[ $? != 0 ]] ; then
+    #install nvim
+    brew install neovim
+fi
+# ensure neovim pip
+pip list | grep 'neovim' &> /dev/null
+if [[ $? != 0 ]]; then
+    pip install neovim
+fi
+
+which -s tmux 
+if [[ $? != 0 ]] ; then
+    brew install tmux
+fi
+
+if [ ! -d $HOME/.oh-my-zsh/custom/themes/powerlevel10k ] ; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
+echo
+echo "    ██╗ ██████╗ ███████╗███╗   ██╗███████╗                                  
+   ██╔╝██╔════╝ ██╔════╝████╗  ██║╚══███╔╝                                  
+  ██╔╝ ██║  ███╗█████╗  ██╔██╗ ██║  ███╔╝                                   
+ ██╔╝  ██║   ██║██╔══╝  ██║╚██╗██║ ███╔╝                                    
+██╔╝   ╚██████╔╝███████╗██║ ╚████║███████╗                                  
+╚═╝     ╚═════╝ ╚══════╝╚═╝  ╚═══╝╚══════╝                                  
+                                                                            
+    ███╗   ███╗███████╗███████╗████████╗███████╗                            
+    ████╗ ████║██╔════╝██╔════╝╚══██╔══╝██╔════╝                            
+    ██╔████╔██║█████╗  █████╗     ██║   ███████╗                            
+    ██║╚██╔╝██║██╔══╝  ██╔══╝     ██║   ╚════██║                            
+    ██║ ╚═╝ ██║███████╗███████╗   ██║   ███████║                            
+    ╚═╝     ╚═╝╚══════╝╚══════╝   ╚═╝   ╚══════╝                            
+                                                                            
+    ████████╗███████╗██████╗ ███╗   ███╗██╗███╗   ██╗ █████╗ ██╗         ██╗
+    ╚══██╔══╝██╔════╝██╔══██╗████╗ ████║██║████╗  ██║██╔══██╗██║        ██╔╝
+       ██║   █████╗  ██████╔╝██╔████╔██║██║██╔██╗ ██║███████║██║       ██╔╝ 
+       ██║   ██╔══╝  ██╔══██╗██║╚██╔╝██║██║██║╚██╗██║██╔══██║██║      ██╔╝  
+       ██║   ███████╗██║  ██║██║ ╚═╝ ██║██║██║ ╚████║██║  ██║███████╗██╔╝   
+       ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝╚═╝    
+                                                                            "
+echo 
+echo "Run checkhealth inside of neovim to ensure plugins installed correctly."
+echo
+echo "There may still be some broken things around here, just try sourcing the
+.zshrc and debugging until it seems like it's fine"
