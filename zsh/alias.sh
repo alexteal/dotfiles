1a7a23a0d735971881a3d816d8b689e99b36243b
alexteal <ateal3235@gmail.com>
2022-10-05 11:02:46 -0400

    moved aliases, added regex infodump

---
+++ zsh/alias.sh
@@ -0,0 +1,66 @@
+#alias ls="ls -G"
+alias snooze="osascript -e 'tell application \"System Events\" to sleep'"
+#set LS colors
+alias ls='ls -GH'
+alias nq='networkquality'
+alias rmpdf='rm2pdf -t ~/projects/remarkable/rm2pdf/templates/A4.pdf'
+alias pomo-clear='rm ~/Library/Application\ Support/pomo/pomo.db; pomo init'
+alias pdf2rm='~/bin/pdf2rmnotebook'
+alias updf2rm='~/.scripts/updf2rm'
+alias jcmp='javac *.java'
+alias jarify='jar -cvf build.jar *' # doesn't work right. I think?
+alias regex='echo "
+POSIX       REGEX           MEANING
+################################################################################
+[:upper:]	[A-Z]	        uppercase letters
+[:lower:]	[a-z]	        lowercase letters
+[:alpha:]	[A-Za-z]	upper- and lowercase letters
+[:digit:]	[0-9]	        digits
+[:xdigit:]	[0-9A-Fa-f]	hexadecimal digits
+[:alnum:]	[A-Za-z0-9]	digits, upper- and lowercase letters
+[:punct:]		        punctuation (all graphic characters except 
+                                letters and digits)
+[:blank:]	[ \\\t]	        space and TAB characters only
+[:space:]	[\\\t\\\n\\\r\\\f\\\v]    blank (whitespace) characters
+[:cntrl:]		        control characters
+[:graph:]	[^ [:cntrl:]]	graphic characters (all characters which have 
+                                graphic representation)
+[:print:]	[[:graph:] ]	graphic characters and space
+[:word:]	[[:alnum:]_]	Alphanumeric characters with underscore character _,
+                                meaning alnum + _. It is a bash specific 
+                                character class.
+"
+'
+alias convert2pdf='/Applications/LibreOffice.app/Contents/MacOS/soffice --headless --convert-to pdf'
+alias gradlew='./gradlew'
+alias ss='source ~/.zshrc'
+# quick edit config files
+alias editnvim="nvim ~/.config/nvim/init.lua"
+alias editzsh="nvim ~/.zshrc"
+alias edittmux="nvim ~/.tmux.conf"
+alias dot="cd ~/projects/dotfiles"
+alias dotconf="cd ~/.config/nvim/"
+alias lg="lazygit"
+
+OS=$( uname -a )
+# unsure if first character is capital or not
+case $OS in 
+    *"ebian"*)
+        ;;
+    *"buntu"*)
+        ;;
+    *"arwin"*)
+        export LS_COLORS=exfxcxdxbxegedabagacad
+        export LSCOLORS=exfxcxdxbxegedabagacad
+        ;;
+    *"edora"*)
+        ;;
+    *"fc36"*) # testing this for now, not sure if it's a good solution.                                    │
+                   # need better portable os type solution
+        ;;
+    *) 
+        export LS_COLORS=exfxcxdxbxegedabagacad
+        export LSCOLORS=exfxcxdxbxegedabagacad
+        ;;
+esac
+
