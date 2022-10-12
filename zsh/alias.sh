b00381aedb16968c7ec0d5b010f05eb167669030
alexteal <ateal3235@gmail.com>
2022-10-12 13:43:17 -0400

    todo for quick open todo doc

--- zsh/alias.sh
+++ zsh/alias.sh
@@ -1,7 +1,30 @@
+OS=$( uname -a )
+# unsure if first character is capital or not
+case $OS in 
+    *"ebian"*)
+        ;;
+    *"buntu"*)
+        ;;
+    *"arwin"*)
+        export LS_COLORS=exfxcxdxbxegedabagacad
+        export LSCOLORS=exfxcxdxbxegedabagacad
+        ;;
+    *"edora"*)
+        ;;
+    *"fc36"*) # testing this for now, not sure if it's a good solution.                                    │
+                   # need better portable os type solution
+        ;;
+    *) 
+        export LS_COLORS=exfxcxdxbxegedabagacad
+        export LSCOLORS=exfxcxdxbxegedabagacad
+        ;;
+esac
+
+
 #alias ls="ls -G"
 alias snooze="osascript -e 'tell application \"System Events\" to sleep'"
 #set LS colors
-#alias ls='ls -GH'
+alias ls='ls -GH'
 alias nq='networkquality'
 alias rmpdf='rm2pdf -t ~/projects/remarkable/rm2pdf/templates/A4.pdf'
 alias pomo-clear='rm ~/Library/Application\ Support/pomo/pomo.db; pomo init'
@@ -9,6 +32,18 @@ alias pdf2rm='~/bin/pdf2rmnotebook'
 alias updf2rm='~/.scripts/updf2rm'
 alias jcmp='javac -g *.java'
 alias jarify='jar -cvf build.jar *' # doesn't work right. I think?
+alias convert2pdf='/Applications/LibreOffice.app/Contents/MacOS/soffice --headless --convert-to pdf'
+alias gradlew='./gradlew'
+alias ss='source ~/.zshrc'
+# quick edit config files
+alias editnvim="nvim ~/.config/nvim/init.lua"
+alias editzsh="nvim ~/.zshrc"
+alias edittmux="nvim ~/.tmux.conf"
+alias dot="cd ~/projects/dotfiles"
+alias dotconf="cd ~/.config/nvim/"
+alias lg="lazygit"
+alias jdbg="java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=5005,suspend=y" #followed by filename
+alias todo='nvim ~/TODO -c ":set filetype=vimwiki"'
 alias regex='echo "
 POSIX       REGEX           MEANING
 ################################################################################
@@ -31,14 +66,3 @@ POSIX       REGEX           MEANING
                                 character class.
 "
 '
-alias convert2pdf='/Applications/LibreOffice.app/Contents/MacOS/soffice --headless --convert-to pdf'
-alias gradlew='./gradlew'
-alias ss='source ~/.zshrc'
-# quick edit config files
-alias editnvim="nvim ~/.config/nvim/init.lua"
-alias editzsh="nvim ~/.zshrc"
-alias edittmux="nvim ~/.tmux.conf"
-alias dot="cd ~/projects/dotfiles"
-alias dotconf="cd ~/.config/nvim/"
-alias lg="lazygit"
-alias jdbg="java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=5005,suspend=y" #followed by filename
