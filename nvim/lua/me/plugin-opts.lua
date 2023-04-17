bef29b45d6242764749ffc1722050b9f1cb98fd8
alexteal <ateal3235@gmail.com>
2023-04-17 11:36:05 -0400

    include clangd lsp

--- nvim/lua/me/plugin-opts.lua
+++ nvim/lua/me/plugin-opts.lua
@@ -28,5 +28,5 @@ require'lspconfig'.dockerls.setup({})
 require'lspconfig'.cmake.setup({})
 require'lspconfig'.tsserver.setup({})
 require'lspconfig'.bashls.setup({})
-
+require'lspconfig'.clangd.setup({})
 require ('mason').setup({})
